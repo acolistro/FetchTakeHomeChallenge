91f554310167c9d057d9cd988ffb29636f2fda44
Alex <alex@mallsoft.love>
2025-03-20 14:02:53 -0700

    Set up CI pipeline

--- app/src/main/java/com/example/fetchtakehomechallenge/repository/ItemRepositoryImpl.kt
+++ app/src/main/java/com/example/fetchtakehomechallenge/repository/ItemRepositoryImpl.kt
@@ -2,8 +2,11 @@ package com.example.fetchtakehomechallenge.repository
 
 import com.example.fetchtakehomechallenge.data.Item
 import com.example.fetchtakehomechallenge.network.ApiService
+import javax.inject.Inject
 
-class ItemRepositoryImpl(private val apiService: ApiService) : ItemRepository {
+class ItemRepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : ItemRepository {
     override suspend fun getItems(): List<Item> {
         return apiService.getItems()
     }
